43c5536974cbc2d2911a467c3068fd8cc62ce824
Taychin <techin.tan@hlabconsulting.com>
2024-03-04 13:06:12 +0700

    add important dependency / init wysiwyg

--- src/WidgetComponent.js
+++ src/WidgetComponent.js
@@ -1,39 +1,84 @@
-import React from 'react';
-import './WidgetComponent.css';
+import React from "react";
+import "./WidgetComponent.css";
+import { ContentState, EditorState, convertToRaw } from "draft-js";
+import { Editor } from "react-draft-wysiwyg";
+import draftToHtml from "draftjs-to-html";
+import "react-draft-wysiwyg/dist/react-draft-wysiwyg.css";
+import { stateFromHTML } from "draft-js-import-html";
 
 class WidgetComponent extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            inputData: props.inputData ?? 0,
-            config: props.config ?? {hideButton: false}
-        };
+  constructor(props) {
+    super(props);
+    console.log("props", props);
+    // add default value
+    let editorState = EditorState.createEmpty();
+    if (props.inputData) {
+      let contentState = stateFromHTML(props.inputData);
+      editorState = EditorState.createWithContent(contentState);
     }
 
-    resetInputData = () => {
-        this.setState({
-            ...this.state ?? {},
-            inputData: 500
-        });
-    }
+    this.state = {
+      editorState: editorState,
+      inputData: props.inputData ?? "",
+      config: props.config ?? { hideButton: false },
+    };
+  }
 
-    updateInputData = (e) => {
-        this.setState({
-            ...this.state,
-            inputData: e.target.value
-        });
-        this.props.outputData(e.target.value);
-    }
+  resetInputData = () => {
+    this.setState({
+      ...(this.state ?? {}),
+      inputData: "hello world",
+      editorState: EditorState.createWithContent(
+        ContentState.createFromText("hello world")
+      ),
+    });
+  };
 
-    render() {
-      return (
-          <div className="WidgetComponent">
-              {!this.state.config?.hideButton && <button onClick={this.resetInputData}>reset input data to 500</button>}
-              <span>this is a widget template. here you see basic input that also outputs data: </span>
-              <input type="number" value={this.state.inputData} onChange={this.updateInputData} />
-          </div>
-      );
-    }
+  updateInputData = (data) => {
+    this.setState({
+      ...this.state,
+      inputData: data,
+    });
+    this.props.outputData(data);
+  };
+
+  onEditorStateChange = (editorState, inputData) => {
+    this.setState({
+      ...this.state,
+      editorState,
+      inputData,
+    });
+    this.props.outputData(inputData);
+  };
+
+  render() {
+    const { editorState } = this.state;
+    return (
+      <div className="WidgetComponent">
+        {!this.state.config?.hideButton && (
+          <button onClick={this.resetInputData}>
+            reset input data to hello world
+          </button>
+        )}
+        <span>
+          this is a widget template. here you see basic input that also outputs
+          data:{" "}
+        </span>
+        <Editor
+          editorState={editorState}
+          toolbarClassName="toolbarClassName"
+          wrapperClassName="wrapperClassName"
+          editorClassName="editorClassName"
+          onEditorStateChange={(e) => {
+            const html = draftToHtml(
+              convertToRaw(e.getCurrentContent())
+            )?.trim();
+            this.onEditorStateChange(e, html);
+          }}
+        />
+      </div>
+    );
+  }
 }
 
 export default WidgetComponent;
--- src/index.js
+++ src/index.js
@@ -1,63 +1,68 @@
-import React from 'react';
-import * as ReactDOM from 'react-dom';
-import WidgetComponent from './WidgetComponent';
+import React from "react";
+import * as ReactDOM from "react-dom";
+import WidgetComponent from "./WidgetComponent";
 
 class WidgetElementWrapper extends HTMLElement {
-    descriptor = {
-        valueModel: {
-            type: 'number',
-            metadata: {title: 'number value'}
-        },
-        configuration: {
-            hideButton: {type: 'boolean', defaultValue: false}
-        }
-    }
-    _config;
-    _inputData;
+  descriptor = {
+    valueModel: {
+      type: "string",
+      metadata: { title: "Note" },
+    },
+    configuration: {
+      hideButton: { type: "boolean", defaultValue: false },
+    },
+  };
+  _config;
+  _inputData;
 
-    get inputData() { return this._inputData; }
-    set inputData(data) {
-        this._inputData = data;
-        this.update();
-    }
+  get inputData() {
+    return this._inputData;
+  }
+  set inputData(data) {
+    this._inputData = data;
+    this.update();
+  }
 
-    get config() { return this._config }
-    set config(config) {
-        this._config = config;
-        this.update();
-    }
+  get config() {
+    return this._config;
+  }
+  set config(config) {
+    this._config = config;
+    this.update();
+  }
 
-    getProps() {
-        return {
-            inputData: this._inputData,
-            config: this._config,
-            outputData: (value) => this.dispatchEvent(new CustomEvent('outputData', {detail: value}))
-        }
-    }
+  getProps() {
+    return {
+      inputData: this._inputData,
+      config: this._config,
+      outputData: (value) =>
+        this.dispatchEvent(new CustomEvent("outputData", { detail: value })),
+    };
+  }
 
-    update() {
-        this.unmount();
-        this.mount();
-    }
+  update() {
+    this.unmount();
+    this.mount();
+  }
 
-    mount() {
-        ReactDOM.render(<WidgetComponent {...this.getProps()} />, this);
-    }
+  mount() {
+    ReactDOM.render(<WidgetComponent {...this.getProps()} />, this);
+  }
 
-    unmount() {
-        ReactDOM.unmountComponentAtNode(this);
-    }
+  unmount() {
+    ReactDOM.unmountComponentAtNode(this);
+  }
 
-    /**
-     * custom elements lifecycle callbacks
-     * https://developer.mozilla.org/en-US/docs/Web/Web_Components/Using_custom_elements#using_the_lifecycle_callbacks
-      */
-    connectedCallback() {
-        this.mount();
-    }
+  /**
+   * custom elements lifecycle callbacks
+   * https://developer.mozilla.org/en-US/docs/Web/Web_Components/Using_custom_elements#using_the_lifecycle_callbacks
+   */
+  connectedCallback() {
+    this.mount();
+  }
 
-    disconnectedCallback() {
-        this.unmount();
-    }
+  disconnectedCallback() {
+    this.unmount();
+  }
 }
-customElements.define('react-template-widget', WidgetElementWrapper);
+customElements.define("react-template-widget", WidgetElementWrapper);
